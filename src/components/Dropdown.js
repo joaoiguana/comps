64a3814e6fdc7eaa5b800a04cf59c1f57717076d
Joao Iguana <jiboiadamor@gmail.com>
2024-10-11 20:38:44 +0100

    practicing props and state design

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,26 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
-function Dropdown({ options }) {
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleOptionClick = () => {
-    console.log('I should close');
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onChange(option);
+    console.log(option.label);
   };
 
   const renderedOptions = options.map((option) => {
-    return <div onClick={handleOptionClick} key={option.value}>
+    return <div className='hover:bg-sky-100 rounded cursor-pointer p-1' onClick={() => handleOptionClick(option)} key={option.value}>
       {option.label}
     </div>
   });
 
-  return <div>
-    <div onClick={handleClick}>Select...</div>
-    {isOpen && <div>{renderedOptions}</div>}
-  </div>
+  return <div ref={divEl} className='w-48 relative'>
+            <Panel className='flex justify-between items-center cursor-pointer' onClick={handleClick}>
+              {value?.label || 'Select...'}
+              <GoChevronDown className='text-lg' />
+            </Panel>
+            {isOpen && (
+              <Panel className='absolute top-full'>
+                {renderedOptions}
+              </Panel>)}
+          </div>
 }
 
 export default Dropdown;
